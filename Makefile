d188d00eb84f2e73fe7e4b0d2e01138bbaceade2
m12watanabe1a <40206149+m12watanabe1a@users.noreply.github.com>
2024-02-23 19:50:07 +0900

    Fix CRC algorithm

--- Makefile
+++ Makefile
@@ -1,3 +1,27 @@
-crc8_checker:
-	g++ $@.cpp -o $@
+######################################
+# target
+######################################
+TARGET = crc8_checker
 
+C_SOURCES =  \
+	crc8_checker.cpp
+
+#######################################
+# paths
+#######################################
+# Build path
+BUILD_DIR = build
+
+all: $(BUILD_DIR)/$(TARGET)
+
+$(BUILD_DIR)/$(TARGET): $(C_SOURCES) Makefile
+	g++ $(C_SOURCES) -o $@
+
+$(BUILD_DIR):
+	mkdir $@
+
+#######################################
+# clean up
+#######################################
+clean:
+	-rm -fR $(BUILD_DIR)
--- crc8_checker.cpp
+++ crc8_checker.cpp
@@ -1,20 +1,47 @@
 #include <vector>
 #include <iostream>
+#include <cstring>
 
-uint8_t calc_crc(const std::string &data)
+static const uint8_t crc8_ccitt_small_table[16] = {
+	0x00, 0x07, 0x0e, 0x09, 0x1c, 0x1b, 0x12, 0x15,
+	0x38, 0x3f, 0x36, 0x31, 0x24, 0x23, 0x2a, 0x2d
+};
+
+uint8_t crc8_ccitt_table(uint8_t val, const void *buf, size_t len)
 {
-  uint8_t ret = 0;
-  for (const auto &c : data)
-  {
-    ret ^= static_cast<uint8_t>(c);
+	size_t i;
+	const uint8_t *p = (uint8_t *)buf;
+
+	for (i = 0; i < len; i++) {
+		val ^= p[i];
+		val = (val << 4) ^ crc8_ccitt_small_table[val >> 4];
+		val = (val << 4) ^ crc8_ccitt_small_table[val >> 4];
+	}
+	return val;
+}
+
+uint8_t crc8_ccitt(uint8_t val, const void *buf, size_t len)
+{
+  const uint8_t poly = 0x07;
+  uint8_t *p = (uint8_t *)buf;
+  for (size_t i = 0; i < len; i++) {
+    val ^= p[i];
+    for (size_t j = 0; j < 8; j++) {
+      if ((val & 0x80) != 0) {
+        val = (uint8_t)((val << 1) ^ poly);
+      } else {
+        val = (val << 1);
+      }
+    }
   }
-  return ret;
+  return val;
 }
 
 int main(int argc, char **argv)
 {
   const std::string data = std::string(argv[1]);
   std::cout << "data: " << data << std::endl;
-  printf("CRC: %02hhX\n", calc_crc(data));
+  printf("CRC: %02hhX\n", crc8_ccitt(0, data.c_str(), data.length()));
+  printf("CRC ccitt: %02hhX\n", crc8_ccitt_table(0, data.c_str(), data.length()));
   return EXIT_SUCCESS;
 }
