569bea17e4a87f83bb3eadec608be0b812452f45
m12watanabe1a <40206149+m12watanabe1a@users.noreply.github.com>
2024-02-24 20:43:21 +0900

    add macro to the implementations

--- crc8_checker.cpp
+++ crc8_checker.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <cstring>
 
+#ifndef USE_TABLE
 static const uint8_t crc8_ccitt_small_table[16] = {
 	0x00, 0x07, 0x0e, 0x09, 0x1c, 0x1b, 0x12, 0x15,
 	0x38, 0x3f, 0x36, 0x31, 0x24, 0x23, 0x2a, 0x2d
@@ -19,7 +20,7 @@ uint8_t crc8_ccitt_table(uint8_t val, const void *buf, size_t len)
 	}
 	return val;
 }
-
+#else
 uint8_t crc8_ccitt(uint8_t val, const void *buf, size_t len)
 {
   const uint8_t poly = 0x07;
@@ -36,6 +37,7 @@ uint8_t crc8_ccitt(uint8_t val, const void *buf, size_t len)
   }
   return val;
 }
+#endif
 
 int main(int argc, char **argv)
 {
